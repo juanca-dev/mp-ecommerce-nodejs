2b42d76cb964359a9b1b959b09ccab4026ae52a4
juanca-dev <juancarlos17.mf@gmail.com>
2021-05-15 21:17:03 -0500

    modificacion de mi repo

--- app.js
+++ app.js
@@ -48,15 +48,15 @@ const metodos_pago = {
 const preferencia = {
   items: [],
   back_urls: {
-      success: "",
-      pending: "",
-      failure:""
+    success: "",
+    pending: "",
+    failure: "",
   },
   payment_methods: metodos_pago,
   payer: cliente,
   auto_return: "approved",
-  notification_url: "",
-  external_reference: "juancarlos17.mf@gmail.com",
+  notification_url: "", // un endpoint en el cual mercado pago nos mandara la actualizacion de nuestro pago mediante un metodo POST y nosotros le tenemos que responder un estado 200 o 201 para que no haga spam a ese endpoint, OJO: no poner la notification_url en modo localhost ya que mp no creara la preferenca por declara una url invalida (127.0.0.1 | localhost)
+  external_reference: "juancarlo17.mf@gmail.com",
 };
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
@@ -88,19 +88,41 @@ app.get("/detail", async function (req, res) {
     currency_id: "PEN",
     unit_price: +price,
   };
-
   preferencia.items.push(item);
-  //ahora modificaremos la back_url para indicar el dominio de  nuestra aplicación
-  preferencia.back_urls.failure= `${req.get("host")}/failure`; // http://127.0.0.1:500
-  preferencia.back_urls.pending= `${req.get("host")}/pending`;
-  preferencia.back_urls.success= `${req.get("host")}/success`;
-  //el notificiation_url solo se puede usar en ambientes de produccion (no localhost ni 127.0.0.1)
-  // porque es a ese endpoint en el cual se mandara el estado de la pasarela de pago y por ende al identificar uno de los 
-  //dominios anteriores lanzara un error y no se procedera con la pasarela
+  // ahora modificamos las back_url para indicar el dominio de nuestra aplicacion
+  preferencia.back_urls.failure = `${req.get("host")}/failure`; // http://127.0.0.1:5000
+  preferencia.back_urls.success = `${req.get("host")}/success`;
+  preferencia.back_urls.pending = `${req.get("host")}/pending`;
+  preferencia.notification_url = `${req.get("host")}/notificaciones`; // esta propiedad se define antes de ya subir a produccion
+  // el notificiation_url solo se puede usar en ambientes de produccion (no localhost ni 127.0.0.1) porque es a ese endpoint en el cual se mandara el estado de la pasarela de pago y por ende al identificar uno de los dominios anteriores lanzara un error y no se procedera con la pasarela
   //   preferencia.notification_url = `${req.get("host")}/notificaciones`;
   const respuesta = await mercadopago.preferences.create(preferencia);
   console.log(respuesta);
+  req.query.init_point = respuesta.body.init_point;
+  req.query.id = respuesta.body.id;
   res.render("detail", req.query);
+});
+
+app.get("/success", function (req, res) {
+  console.log(req.query);
+  res.render("success", req.query);
+});
+
+app.get("/failure", function (req, res) {
+  res.render("failure", req.query);
+});
+
+app.get("/pending", function (req, res) {
+  res.render("pending", req.query);
+});
+
+app.post("/notificaciones", function (req, res) {
+  console.log("INICIO DE NOTIFICACIONES");
+  console.log("MEDIANTE EL QUERY PARAMS");
+  console.log(req.query);
+  console.log("MEDIANTE EL BODY");
+  console.log(req.body);
+  res.status(200);
 });
 
 app.listen(port);
